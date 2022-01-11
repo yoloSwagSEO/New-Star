97525317e9445a55f1d54e4e84f5b2213f7b9ae3
Florian <erosdevfr@gmail.com>
2022-01-11 11:45:44 +0100

    [ADD] MISSION ENUMERATION class

---
+++ includes/enums/MissionsEnum.php
@@ -0,0 +1,20 @@
+<?php
+namespace Florian\NewStar\enums;
+
+class MissionsEnum
+{
+    public const ATTAQUE = 1;
+    public const ACS = 2;
+    public const TRANSPORT = 3;
+    public const STATIONE = 4;
+    public const TRANSFER = 5;
+    public const SPY = 6;
+    public const COLONIZE = 7;
+    public const RECICLE = 8;
+    public const DESTROY = 9;
+    public const MISSILE = 10;
+    public const PROSPECT = 11;
+    public const EXPEDITION = 15;
+    public const WAR_EXPEDITION = 18;
+
+}
