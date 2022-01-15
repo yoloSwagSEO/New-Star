5066b6725b0d232abb5aeefec29d0d27aa76b7c0
Florian LAFFONT <erosdevfr@gmail.com>
2022-01-15 15:33:29 +0100

    [UPT] Optimise retriaving User

--- includes/models/User.php
+++ includes/models/User.php
@@ -5,5 +5,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-
+    public function messages()
+    {
+        return $this->hasMany(Message::class,'message_owner','id');
+    }
 }
